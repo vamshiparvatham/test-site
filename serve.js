dbf1cc021a34db39af29780b95f24d5aa5656db7
vamshiparvatham <parvathamvamshi0105@gmail.com>
2026-04-08 22:51:43 +0530

    Initial commit: FlowBoard test site

---
+++ serve.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3333;
+const DIR = __dirname;
+
+const MIME_TYPES = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".xml": "application/xml",
+  ".txt": "text/plain",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(DIR, req.url === "/" ? "index.html" : req.url);
+
+  const ext = path.extname(filePath);
+  const contentType = MIME_TYPES[ext] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
+      return;
+    }
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Test site running at http://localhost:${PORT}`);
+  console.log("Press Ctrl+C to stop");
+});
